fdf1666d3b26f2f5885687a167325aafb2bd1194
Will Scullin <willscullin@google.com>
2023-06-09 15:02:07 -0700

    Add malloy-service as a submodule

--- scripts/gen-protos.sh
+++ scripts/gen-protos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python -m grpc_tools.protoc -I../malloy-service/protos --python_out=src/malloy --pyi_out=src/malloy --grpc_python_out=src/malloy ../malloy-service/protos/**/**/*.proto
+python -m grpc_tools.protoc -I./submodules/malloy-service/protos --python_out=src/malloy --pyi_out=src/malloy --grpc_python_out=src/malloy ./submodules/malloy-service/protos/**/**/*.proto
 
 echo TODO: Fix src/malloy/services/v1/compiler_pb2_grpc.py import namespace automatically
