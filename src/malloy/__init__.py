e476f34ebdcb80cf54c58660f9c3890711d9f067
Michael Aden <maden@google.com>
2023-07-12 10:03:47 -0500

    bump version 2023.1014 -> 2023.1015

--- src/malloy/__init__.py
+++ src/malloy/__init__.py
@@ -22,7 +22,7 @@
 # __init__.py
 """Base module of the Malloy python runtime."""
 # Version of the python malloy package
-__version__ = "2023.1014"
+__version__ = "2023.1015"
 
 from malloy.runtime import (Runtime)
 try:
