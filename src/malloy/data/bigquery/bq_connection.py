eef2022dfabe20f0c9e23b821075dbf938085ea9
Michael Aden <maden@google.com>
2023-11-30 14:03:22 -0600

    "fix" pylint circular references

--- src/malloy/data/bigquery/bq_connection.py
+++ src/malloy/data/bigquery/bq_connection.py
@@ -26,15 +26,21 @@ from __future__ import annotations
 
 from ..connection import ConnectionInterface
 
-import malloy
-import platform
+import importlib
 
 from absl import logging
 from collections.abc import Sequence
 from google.cloud import bigquery
 from google.api_core.gapic_v1 import client_info
 
-MALLOY_USER_AGENT = f"malloy-{malloy.__version__}_{platform.python_version()}"
+
+def set_malloy_user_agent():
+  mod = importlib.import_module("malloy")
+  version = getattr(mod, "__version__")
+  return f"malloy/{version}"
+
+
+MALLOY_USER_AGENT = set_malloy_user_agent()
 
 
 class BigQueryConnection(ConnectionInterface):
