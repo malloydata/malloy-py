fd74ee30171f222792b0d06729eb7bbe686150c7
Will Scullin <willscullin@google.com>
2024-03-20 09:30:43 -0700

    Add github testing action.

--- scripts/gen-protos.sh
+++ scripts/gen-protos.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 python -m grpc_tools.protoc -I./submodules/malloy-service/protos --python_out=src/malloy --pyi_out=src/malloy --grpc_python_out=src/malloy ./submodules/malloy-service/protos/**/**/*.proto
 sed -i '' -e "s/from services.v1/from malloy.services.v1/g" src/malloy/services/v1/compiler_pb2_grpc.py
