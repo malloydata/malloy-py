e95853c4e1214c0720d8c0422dc40eef7f93e746
Will Scullin <willscullin@google.com>
2024-03-28 10:16:48 -0700

    bump version 2024.1079-dev -> 2024.1080-dev

--- src/malloy/__init__.py
+++ src/malloy/__init__.py
@@ -22,7 +22,7 @@
 # __init__.py
 """Base module of the Malloy python runtime."""
 # Version of the python malloy package
-__version__ = "2024.1079-dev"
+__version__ = "2024.1080-dev"
 
 import importlib
 
